65308a8aff83212737304f5abea5066ddb4c4f6e
Matias Crespi <mat.cres@gmail.com>
2018-03-02 18:26:34 -0300

    remove comments

--- app/main.js
+++ app/main.js
@@ -19,27 +19,3 @@ class Root extends Component {
 }
 
 ReactDOM.render(<Root/>, document.getElementById('root'))
-
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import { AppContainer, } from 'react-hot-loader'
-//
-// import Root from './routes/Root'
-//
-// const render = (Component) => {
-//   ReactDOM.render(
-//       <AppContainer>
-//         <Component />
-//       </AppContainer>,
-//       document.getElementById('root'),
-//   )
-// }
-//
-// render(Root)
-//
-// if (module.hot) {
-//   module.hot.accept('./routes/Root', () => {
-//     const newApp = Root
-//     render(newApp)
-//   })
-// }
